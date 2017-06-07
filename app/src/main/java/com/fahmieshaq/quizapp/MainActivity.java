51c78021fc778a28bae6d65993cfa9a282f42e16
Fahmi Eshaq <fjamcom@gmail.com>
2017-06-07 07:54:21 -0400

    Added styles.xml for Arabic locale

--- app/src/main/java/com/fahmieshaq/quizapp/MainActivity.java
+++ app/src/main/java/com/fahmieshaq/quizapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.fahmieshaq.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selectedButton(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
     }
 }
